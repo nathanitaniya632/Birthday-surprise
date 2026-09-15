1944066c42738c5a294af38406ba2eee271e93f5
Taniya Nathani <nathanitaniya632@gmail.com>
2026-09-15 18:32:28 +0530

    Fix music and photos

--- src/App.js
+++ src/App.js
@@ -209,7 +209,7 @@ function App() {
         {/* SAME AUDIO ELEMENT */}
         <audio
           ref={audioRef}
-         src="./birthday-music.mp3"
+         src="./birthday-music.mp3.mp3"
           loop
           preload="auto"
         />
@@ -301,7 +301,7 @@ function App() {
       {/* MUSIC CONTINUES AFTER UNLOCK */}
       <audio
         ref={audioRef}
-        src="/birthday-music.mp3"
+        src="/birthday-music.mp3.mp3"
         loop
         preload="auto"
       />
